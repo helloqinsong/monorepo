8238349cf830a9b55b1dc193508f73311ddc1a4f
qinsong <qinsong_king@163.com>
2024-10-23 22:04:15 +0800

    更新乘法

--- packages/util/lib/index.ts
+++ packages/util/lib/index.ts
@@ -2,4 +2,4 @@ export const sum = (a: number, b: number) => a + b + 1;
 
 export const desc = (a: number, b: number) => a - b + 1;
 
-export const cc = (a: number, b: number) => a * b + 1;
+export const cc = (a: number, b: number) => a * b + 2;
